71164233f3d6c33ed1a33f39356a21aa36065758
Marco <mmarco.antvaz@gmail.com>
2024-01-09 21:29:07 -0600

    adding some sty

--- app/index.js
+++ app/index.js
@@ -1,6 +1,48 @@
+
+window.addEventListener('load', function () {
+
+  const history = setValues()
+
+  // const history = document.querySelector('#history')
+
+  // // setInterval(getData, 1000)
+  // setInterval(function () {
+  //   history.appendChild(createHistoryElement(Math.random() * 100))
+  //   history.removeChild(history.firstChild)
+  // }, 2000)
+
+
+  this.setInterval(function () {
+    const newValue = Math.random() * 100
+    history.forEach(function (el, idx) {
+      if (idx < history.length - 1) {
+        el.style.height = history[idx + 1].style.height
+      } else {
+        el.style.height = newValue + '%'
+      }
+    })
+  }, 1000)
+
+})
+
 async function getData() {
-  const data = (await fetch('data')).json()
-  console.log(data)
+  // const data = (await fetch('data')).json()
+  // console.log(data)
 }
 
-setInterval(getData, 60000)
+function setValues() {
+  const h = document.querySelectorAll('.component-sensor-history-item')
+
+  return Array.from(h).map(function (el) {
+    // console.log(el)
+    el.style.height = (Math.random() * 100) + '%'
+    return el
+  })
+}
+
+function createHistoryElement(value) {
+  const el = document.createElement('div')
+  el.className = 'component-sensor-history-item'
+  el.style.height = value + '%'
+  return el
+}
